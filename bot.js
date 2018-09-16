896701eda5911053c28005f8a47506e0352e60b5
abodeo32 <43319927+abodeo32@users.noreply.github.com>
2018-09-16 19:29:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'برفكس بوتك'
+const prefix = 'd'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
